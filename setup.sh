63d8f0973daa57e7463fd3abc849582dab1b35bf
Shivank Dubey <shivank.dubey@dolby.com>
2022-10-18 13:59:32 +1100

    testing code

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 touch .env
 
+echo "-------";
+echo $stream_name | sed 's/./& /g'
+echo $publishing_token | sed 's/./& /g'
+echo $account_id | sed 's/./& /g'
+echo "-------";
 echo VITE_MILLICAST_STREAM_NAME=$stream_name >> .env
 echo VITE_MILLICAST_STREAM_PUBLISHING_TOKEN=$publishing_token >> .env
 echo VITE_MILLICAST_STREAM_ACCOUNT_ID=$account_id >> .env
