9c35c508354641ab4d860c5b24d624479a9375fe
wenfeng song <wenfeng.song@dolby.com>
2022-10-18 15:36:36 +1100

    move env output to sh

--- setup.sh
+++ setup.sh
@@ -5,3 +5,5 @@ echo VITE_MILLICAST_STREAM_NAME=$stream_name >> .env
 echo VITE_MILLICAST_STREAM_PUBLISHING_TOKEN=$publishing_token >> .env
 echo VITE_MILLICAST_STREAM_ACCOUNT_ID=$account_id >> .env
 
+cat .env
+
