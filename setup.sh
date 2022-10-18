b76a9db41daf09ee215395071cb14666258a156a
Shivank Dubey <shivank.dubey@dolby.com>
2022-10-18 13:11:41 +1100

    Try out secrets this way

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 touch .env
-echo VITE_MILLICAST_STREAM_NAME=${{ secrets.MILLICAST_STREAM_NAME }} >> .env
-echo VITE_MILLICAST_STREAM_PUBLISHING_TOKEN=${{ secrets.STREAM_PUBLISHING_TOKEN }} >> .env
-echo VITE_MILLICAST_STREAM_ACCOUNT_ID=${{secrets.STREAM_ACCOUNT_ID }} >> .env
+echo VITE_MILLICAST_STREAM_NAME=$stream_name >> .env
+echo VITE_MILLICAST_STREAM_PUBLISHING_TOKEN=$publishing_token >> .env
+echo VITE_MILLICAST_STREAM_ACCOUNT_ID=$account_id >> .env
+
+cat .env
