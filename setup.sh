9fc47faa3be1cb1d1da865151d5c79a5f0506d48
Shivank Dubey <shivank.dubey@dolby.com>
2022-10-18 13:27:07 +1100

    remove cat at the end

--- setup.sh
+++ setup.sh
@@ -3,5 +3,3 @@ touch .env
 echo VITE_MILLICAST_STREAM_NAME=$stream_name >> .env
 echo VITE_MILLICAST_STREAM_PUBLISHING_TOKEN=$publishing_token >> .env
 echo VITE_MILLICAST_STREAM_ACCOUNT_ID=$account_id >> .env
-
-cat .env
