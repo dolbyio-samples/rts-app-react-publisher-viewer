4064d162fed6f6eb6dd6667f02727f3f978ce5e9
Shivank Dubey <shivank.dubey@dolby.com>
2022-10-18 13:38:34 +1100

    remove echo

--- setup.sh
+++ setup.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 touch .env
 
-echo "------"
-echo $stream_name
-echo $publishing_token
-echo $account_id
-echo "------"
-
-
 echo VITE_MILLICAST_STREAM_NAME=$stream_name >> .env
 echo VITE_MILLICAST_STREAM_PUBLISHING_TOKEN=$publishing_token >> .env
 echo VITE_MILLICAST_STREAM_ACCOUNT_ID=$account_id >> .env
