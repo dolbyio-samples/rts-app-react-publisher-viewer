3949b6321d9953cda5aa19c5b6fe0fce9588f018
Shivank Dubey <shivank.dubey@dolby.com>
2022-10-18 16:07:07 +1100

    Remove dotenv and console logs

--- src/App.tsx
+++ src/App.tsx
@@ -30,11 +30,6 @@ function App() {
   useEffect(() => {
     setAccessToken(import.meta.env.VITE_MILLICAST_STREAM_PUBLISHING_TOKEN);
     setStreamId(import.meta.env.VITE_MILLICAST_STREAM_NAME);
-
-    // TODO remove after complete
-    console.log("Test, remove after validation complete");
-    console.log(accessToken, streamId);
-    console.log("------");
   }, []);
 
 
