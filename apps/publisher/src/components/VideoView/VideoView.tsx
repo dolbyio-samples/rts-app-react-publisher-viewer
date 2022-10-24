3e693e5919a0a8cb17750a69bdd6f28a1c6402b2
Ma, Ray <Ray.Ma@dolby.com>
2022-10-24 15:49:46 +1100

    Added hover action for the fullscreen button. Updated video fullscreen style

--- apps/publisher/src/components/VideoView/VideoView.tsx
+++ apps/publisher/src/components/VideoView/VideoView.tsx
@@ -12,6 +12,7 @@ const VideoView = ({mediaStream}: VideoViewProps) => {
     const fullScreenButton = useRef<HTMLButtonElement>(null);
 
     const [ isFullScreenOn, setFullScreenStatus ] = useState(false);
+    const [ videoIsHovered, setVideoHoverStatus ] = useState(false);
 
     useEffect(() => {
         if (video.current && mediaStream) {
@@ -20,15 +21,31 @@ const VideoView = ({mediaStream}: VideoViewProps) => {
     }, [mediaStream]);
 
     useEffect(() => {
+        fullScreenButton.current?.classList.add('icon-button--on')
         setTimeout(() => {
-            fullScreenButton.current?.classList.remove('icon-button--init-status');
+            fullScreenButton.current?.classList.remove('icon-button--on');
         }, 2000)
     }, [])
 
+    const VideoViewWrapper = (isFullScreenOn: boolean) => {
+        if (isFullScreenOn) {
+            return "absolute";
+        }
+
+        return "relative";
+    }
+
     return (
-        <Box test-id="video-view-wrapper" sx={{ '.video': { transform: 'scaleX(-1)' }, '.video--fullscreen': { width: '100vw', height: '100vh', overflowY: 'hidden'}, '.icon-button--init-status': {background: 'white', border: '1px solid black'}}} pos="relative">
+        <Box 
+            test-id="video-view-wrapper" 
+            sx={{ '.video': { transform: 'scaleX(-1)' }, '.video--fullscreen': { width: '100vw', height: '100vh', overflowY: 'hidden'}, '.icon-button--on': {background: 'white', border: '1px solid black'}}} 
+            pos={VideoViewWrapper(isFullScreenOn)} 
+            bg="black" 
+            top="0" 
+            right="0" 
+            zIndex="1">
             {/* eslint-disable-next-line react/no-unknown-property */}
-            <video className={`video ${isFullScreenOn && 'video--fullscreen'}`} playsInline test-id="video-view" autoPlay ref={video} muted />
+            <video className={`video ${isFullScreenOn && 'video--fullscreen'}`} playsInline test-id="video-view" autoPlay ref={video} muted onMouseMove={() => setVideoHoverStatus(true)} onMouseOut={() => setVideoHoverStatus(false)}/>
             <IconButton 
                 aria-label="Full screen" 
                 p="10px" 
@@ -39,7 +56,7 @@ const VideoView = ({mediaStream}: VideoViewProps) => {
                 bottom="0" 
                 borderRadius="0"
                 border="1px solid transparent"
-                className="icon-button--init-status"
+                className={`${videoIsHovered && "icon-button--on"}`}
                 _hover={{bg: 'white', border: '1px solid black'}}
                 ref={fullScreenButton}
                 onClick={() => setFullScreenStatus(!isFullScreenOn)}
