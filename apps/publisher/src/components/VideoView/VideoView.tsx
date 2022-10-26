1a86c2c79f0c79504f2df581af4f22c5695ee64a
Ma, Ray <Ray.Ma@dolby.com>
2022-10-26 13:45:42 +1100

    Fixed button position issue on mobile

--- apps/publisher/src/components/VideoView/VideoView.tsx
+++ apps/publisher/src/components/VideoView/VideoView.tsx
@@ -40,7 +40,12 @@ const VideoView = ({mediaStream, statistic}: VideoViewProps) => {
             padding: "10px",
             background: "transparent",
             borderRadius: "0",
-            border: "1px solid transparent"
+            border: "1px solid transparent",
+            width: "min-content",
+        },
+        '.icon-button--video-on-hover': {
+            background: 'white',
+            border: '1px solid black'
         },
         '.icon-button: hover': {
             background: 'white',
@@ -61,22 +66,22 @@ const VideoView = ({mediaStream, statistic}: VideoViewProps) => {
             {/* eslint-disable-next-line react/no-unknown-property */}
             <video className={`video ${isFullScreen && 'video--fullscreen'}`} playsInline test-id="video-view" autoPlay ref={video} muted onMouseOver={() => setIsHoveredOnVideo(true)} onMouseOut={() => setIsHoveredOnVideo(false)}/>
             {showStatisticInfo && <StatisticInfo statistic={statistic} />}
-            <HStack pos="absolute" bottom="0" right="0">
-            <IconButton 
-                aria-label="Full screen" 
-                size="md"
-                className={`icon-button ${isHoveredOnVideo && "icon-button--hover"}`}
-                ref={fullScreenButton}
-                onClick={() => setIsFullScreen(!isFullScreen)}
-                icon={isFullScreen ? <FullScreenExit fill="black" /> : <FullScreen fill="black" />}
-                onMouseOver={() => setIsHoveredOnVideo(true)} 
-                onMouseOut={() => setIsHoveredOnVideo(false)}/>
-            <IconButton 
-                aria-label="Stream Information" 
-                size="md"
-                className="icon-button"
-                onClick={() => setshowStatisticInfo(!showStatisticInfo)}
-                icon={<Info fill="black" />}/>
+            <HStack pos="absolute" bottom="0" right="0" spacing="0">
+                <IconButton 
+                    aria-label="Full screen" 
+                    size="md"
+                    className={`icon-button ${isHoveredOnVideo && "icon-button--video-on-hover"}`}
+                    ref={fullScreenButton}
+                    onClick={() => setIsFullScreen(!isFullScreen)}
+                    icon={isFullScreen ? <FullScreenExit fill="black" /> : <FullScreen fill="black" />}
+                    onMouseOver={() => setIsHoveredOnVideo(true)} 
+                    onMouseOut={() => setIsHoveredOnVideo(false)}/>
+                <IconButton 
+                    aria-label="Stream Information" 
+                    size="md"
+                    className="icon-button"
+                    onClick={() => setshowStatisticInfo(!showStatisticInfo)}
+                    icon={<Info fill="black" />}/>
             </HStack>
         </Box>
     );
