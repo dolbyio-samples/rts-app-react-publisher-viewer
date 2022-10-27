725a0b8e2269126234139cfd1983aea12ae253cd
wenfeng song <wenfeng.song@dolby.com>
2022-10-28 09:47:52 +1100

    manual deploy to netlify

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ # TODO: deploy different app depends on what files are changed in commit
+command='DEBUG=* yarn netlify deploy --auth $NETLIFY_AUTH_TOKEN --dir=apps/publisher/dist --site=astounding-lily-fa33fc'
+if [[ $BRANCH_REF == 'refs/heads/main' ]]
+then
+command="${command}  --prod"
+else
+command="${command}  --alias=$PR_TITLE"
+fi
+eval $command
