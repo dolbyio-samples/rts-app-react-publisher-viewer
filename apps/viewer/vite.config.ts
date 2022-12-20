9df25464d385e917bb5b23afea3a1c78ed7dab6a
Kharge <vishal.kharge@dolby.com>
2022-12-21 10:01:31 +1100

    Fix vite gateway timeout issue

--- apps/viewer/vite.config.ts
+++ apps/viewer/vite.config.ts
@@ -5,6 +5,9 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [tsconfigPaths(), react(), eslint()],
+  resolve: {
+    preserveSymlinks: true,
+  },
   define: {
     __APP_VERSION__: JSON.stringify('0.0.1'),
   },
