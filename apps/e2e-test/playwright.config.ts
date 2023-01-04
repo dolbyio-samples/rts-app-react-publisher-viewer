2aca06222114610696ae441eccb53fb18556c03e
Kharge <vishal.kharge@dolby.com>
2023-01-04 16:03:38 +1100

    Draft: Test - ReadMe

--- apps/e2e-test/playwright.config.ts
+++ apps/e2e-test/playwright.config.ts
@@ -53,5 +53,5 @@ export const options: PlaywrightOptions = {
   },
   contextOptions: browserContextOptions,
   reportPath: process.env.REPORT_PATH || defaultReportPath,
-  dynamicStreamName: false,
+  dynamicStreamName: process.env.DYNAMIC_STREAM_NAME?.toLowerCase() === 'true' || false,
 };
