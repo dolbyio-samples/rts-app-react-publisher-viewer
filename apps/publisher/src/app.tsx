7db55cf2aeac1b4cd6c82560f42ac3b63387b6f5
Jack Shen <git@jackshen.me>
2023-03-28 18:43:20 +1100

    temp: test request permissions

--- apps/publisher/src/app.tsx
+++ apps/publisher/src/app.tsx
@@ -1,6 +1,8 @@
+import ActionBar from './components/action-bar';
+import PublisherVideoTiles from './components/publisher-video-tiles';
+import useMultiMediaStreams, { StreamTypes } from './hooks/use-multi-media-streams';
+import './styles/app.css';
 import { Box, Flex, Text, VStack } from '@chakra-ui/react';
-import React, { useEffect, useState } from 'react';
-
 import InfoBar from '@millicast-react/info-bar';
 import InfoLabel from '@millicast-react/info-label';
 import useLocalFiles from '@millicast-react/use-local-files';
@@ -9,12 +11,7 @@ import useNotification from '@millicast-react/use-notification';
 import usePageClosePrompt from '@millicast-react/use-page-close-prompt';
 import usePublisher from '@millicast-react/use-publisher';
 import useScreenShare from '@millicast-react/use-screen-share';
-
-import ActionBar from './components/action-bar';
-import PublisherVideoTiles from './components/publisher-video-tiles';
-import useMultiMediaStreams, { StreamTypes } from './hooks/use-multi-media-streams';
-
-import './styles/app.css';
+import React, { useEffect, useState } from 'react';
 
 const TIMESTAMP_STREAM_NAME = new Date().valueOf().toString();
 
@@ -65,6 +62,10 @@ const App = () => {
     const [initVideoDevice] = mediaDevices.videoDevices;
 
     if (initAudioDevice && initVideoDevice) {
+      console.log({
+        audioDeviceId: initAudioDevice.deviceId,
+        videoDeviceId: initVideoDevice.deviceId,
+      });
       mediaDevices.start({
         audioDeviceId: initAudioDevice.deviceId,
         videoDeviceId: initVideoDevice.deviceId,
