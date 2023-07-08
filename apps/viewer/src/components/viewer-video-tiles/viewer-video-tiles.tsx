dfc255dd370d658aa2aebf7c8b2c539590ea9825
Ramesh Prasad <ramesh.prasad@dolby.com>
2023-07-08 15:53:52 +1000

    viewer

--- apps/viewer/src/components/viewer-video-tiles/viewer-video-tiles.tsx
+++ apps/viewer/src/components/viewer-video-tiles/viewer-video-tiles.tsx
@@ -23,16 +23,32 @@ const ViewerVideoTiles = ({
 
   const [mainSourceId, setMainSourceId] = useState('');
 
+  const isMobileBrowser =
+  (typeof navigator !== 'undefined' && navigator.userAgent.match(/Android|iPhone|mobile/i)) ||
+  /*
+   * Apple tablets are faking userAgent by default to treat them as desktop browsers, additional check for Apple devices
+   */
+  (navigator?.platform === 'MacIntel' && 'maxTouchPoints' in navigator && navigator.maxTouchPoints > 0);
+
+  const [isMobile, setIsMobile] = useState(false);
+  const [isMobileSmall, setIsMobileSmall] = useState(false);
+  const [isTablet, setIsTablet] = useState(false);
+  const [isDesktop, setIsDesktop] = useState(!isMobileBrowser);
+  
   // Assign the first source as the initial main stream
   useEffect(() => {
-    if (remoteTrackSources.size && (mainSourceId === '' || !remoteTrackSources.get(mainSourceId))) {
-      const [[firstSourceId]] = remoteTrackSources;
+    if (remoteTrackSources.size) {
+      const srcArray = Array.from(remoteTrackSources);
+      const newSrcId = srcArray[srcArray.length -1][0];
 
-      setMainSourceId(firstSourceId);
-      changeMainSource(firstSourceId);
+      console.log(">>>>", isDesktop, newSrcId);
+      if(remoteTrackSources.size == 1 || (isDesktop && newSrcId.toLowerCase().includes('desktop'))){
+        setMainSourceId(newSrcId);
+        changeMainSource(newSrcId);
+      }
     }
   }, [remoteTrackSources.size]);
-
+  
   // Reset main stream when layers change
   useEffect(() => {
     if (mainSourceId === '') {
@@ -74,6 +90,48 @@ const ViewerVideoTiles = ({
 
   const isStreaming = remoteTrackSources.size > 0;
 
+  const breakpoints = {
+    mobileSmall: 320,
+    mobile: 600,
+    tablet: 1024,
+    desktop: 1440,
+  };
+
+  const handleDeviceType = () => {
+    let mainDimension = window.innerWidth;
+
+    if (window.innerWidth > window.innerHeight) {
+      mainDimension = window.innerHeight;
+    }
+    if (window.innerWidth <= breakpoints.tablet || isMobileBrowser) {
+      if (mainDimension <= breakpoints.mobileSmall) {
+        setIsMobile(false);
+        setIsMobileSmall(true);
+        setIsTablet(false);
+        setIsDesktop(false);
+      } else if (mainDimension <= breakpoints.mobile) {
+        setIsMobile(true);
+        setIsMobileSmall(false);
+        setIsTablet(false);
+        setIsDesktop(false);
+      } else if (mainDimension <= breakpoints.tablet || isMobileBrowser) {
+        setIsMobile(false);
+        setIsMobileSmall(false);
+        setIsTablet(true);
+        setIsDesktop(false);
+      }
+    } else {
+      setIsMobile(false);
+      setIsMobileSmall(false);
+      setIsTablet(false);
+      setIsDesktop(true);
+    }
+  };
+
+  useEffect(() => {
+    handleDeviceType();
+  }, []);
+
   return (
     <Flex alignItems="center" flex={1} justifyContent="center" width="100%">
       {!isStreaming || mainSourceId === '' ? (
@@ -110,35 +168,6 @@ const ViewerVideoTiles = ({
               }}
             />
           </Box>
-          {remoteTrackSources.size > 1 ? (
-            <VStack height="100%" maxWidth="20vw">
-              {Array.from(remoteTrackSources)
-                .filter(([sourceId]) => sourceId !== mainSourceId)
-                .map(([sourceId, { mediaStream }]) => (
-                  <Box
-                    cursor="pointer"
-                    height={`calc(100% / (${MAX_SOURCES} - 1))`}
-                    key={sourceId}
-                    onClick={() => {
-                      changeMainSource(sourceId);
-                    }}
-                    test-id="rtsVideo"
-                    width="100%"
-                  >
-                    <ViewerVideoView
-                      controls={viewerPlaybackControl[sourceId]}
-                      isStreaming={isStreaming}
-                      videoProps={{
-                        displayVideo: true,
-                        label: sourceId,
-                        mediaStream,
-                        muted: true,
-                      }}
-                    />
-                  </Box>
-                ))}
-            </VStack>
-          ) : undefined}
         </HStack>
       )}
     </Flex>
