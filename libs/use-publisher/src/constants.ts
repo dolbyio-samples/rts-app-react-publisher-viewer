54eed3fa81ebd486c280126a30239af6b5dd9470
Guilherme Gambeti <guilherme.gambeti@dolby.com>
2023-08-15 18:56:48 +1000

    added a new bitrate option for 4mb; added .preventDefault() to render closing webpage warning message

--- libs/use-publisher/src/constants.ts
+++ libs/use-publisher/src/constants.ts
@@ -2,6 +2,7 @@ import { Bitrate } from './types';
 
 export const bitrateList: Bitrate[] = [
   { name: 'Auto', value: 0 },
+  { name: '4 Mbps', value: 4_000 },
   { name: '2 Mbps', value: 2_000 },
   { name: '1 Mbps', value: 1_000 },
   { name: '500 kbps', value: 500 },
