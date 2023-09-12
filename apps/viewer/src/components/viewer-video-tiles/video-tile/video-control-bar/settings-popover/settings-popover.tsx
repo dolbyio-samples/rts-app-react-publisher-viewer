1d0190cacf525c01921a5c89999093690a5b5a63
Guilherme Gambeti <guilherme.gambeti@dolby.com>
2023-09-13 09:40:39 +1000

    added source selection item to settings menu

--- apps/viewer/src/components/viewer-video-tiles/video-tile/video-control-bar/settings-popover/settings-popover.tsx
+++ apps/viewer/src/components/viewer-video-tiles/video-tile/video-control-bar/settings-popover/settings-popover.tsx
@@ -1,14 +1,14 @@
 import { Box } from '@chakra-ui/react';
 import React from 'react';
 
-import { IconCameraOn, IconSettings } from '@millicast-react/dolbyio-icons';
-import Dropdown from '@millicast-react/dropdown';
+import { IconCameraOn, IconSettings, Sliders } from '@millicast-react/dolbyio-icons';
+import Dropdown, { Element } from '@millicast-react/dropdown';
 import Popover from '@millicast-react/popover';
 
 import { SettingsPopoverProps } from './types';
 import { qualityElementResolver } from './utils';
 
-const SettingsPopover = ({ iconProps, quality }: SettingsPopoverProps = {}) => (
+const SettingsPopover = ({ iconProps, quality, sources }: SettingsPopoverProps = {}) => (
   <Popover
     heading="Stream settings"
     icon={<IconSettings fill="white" />}
@@ -22,7 +22,7 @@ const SettingsPopover = ({ iconProps, quality }: SettingsPopoverProps = {}) => (
           disabled={quality.isDisabled}
           elementResolver={qualityElementResolver}
           elementsList={quality.options}
-          leftIcon={<IconCameraOn />}
+          leftIcon={<Sliders />}
           onSelect={quality.handleSelect}
           placeholder="Quality"
           selected={quality.value}
@@ -30,6 +30,21 @@ const SettingsPopover = ({ iconProps, quality }: SettingsPopoverProps = {}) => (
         />
       </Box>
     ) : undefined}
+
+    {sources && (
+      <Box marginTop="20px">
+        <Dropdown
+          disabled={sources.isDisabled}
+          elementResolver={(e: unknown) => e as Element}
+          elementsList={sources.options}
+          leftIcon={<IconCameraOn />}
+          onSelect={sources.handleSelect}
+          placeholder="Source"
+          selected={sources.value}
+          testId="videoSourcesSelect"
+        />
+      </Box>
+    )}
   </Popover>
 );
 
--- apps/viewer/src/components/viewer-video-tiles/video-tile/video-control-bar/settings-popover/types.ts
+++ apps/viewer/src/components/viewer-video-tiles/video-tile/video-control-bar/settings-popover/types.ts
@@ -2,13 +2,15 @@ import { IconButtonProps } from '@chakra-ui/react';
 
 import { SimulcastQuality } from '@millicast-react/use-viewer';
 
+interface SettingItem<T> {
+  handleSelect: (data: unknown) => void;
+  isDisabled?: boolean;
+  isHidden?: boolean;
+  options: T;
+  value: string;
+}
 export interface SettingsPopoverProps {
   iconProps?: Omit<IconButtonProps, 'aria-label'>;
-  quality?: {
-    handleSelect: (data: unknown) => void;
-    isDisabled?: boolean;
-    isHidden?: boolean;
-    options: SimulcastQuality[];
-    value: string;
-  };
+  quality?: SettingItem<SimulcastQuality[]>;
+  sources?: SettingItem<{ id: string; label: string; data: { sourceId: string } }[]>;
 }
